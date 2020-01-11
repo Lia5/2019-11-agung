9ae5472473fd34bad2aa15e900a0848081e75c34
Helen <liaforweb@gmail.com>
2020-01-11 04:55:14 +0200

    prav

--- src/modules/load.js
+++ src/modules/load.js
@@ -2,6 +2,7 @@ export default class LoadData {
   load(ms) {
     return new Promise(function (resolve, reject) {
       setTimeout(resolve, ms);
+      document.getElementById('header').style.height = window.innerHeight;
     });
   }
 }
